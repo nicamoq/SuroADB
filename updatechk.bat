888082f047ddb2ccc840558f0069547b9c2a76a5
Kutsuro Taikon <Rythmnirvana@gmail.com>
2018-11-01 16:41:41 +0800

    updatechk.bat
    Main server for latest SuroADB versions.
    Current:
    set newver=12
    set rawver=12
    set linea===========================================================
    set lineb=Notes:
    set linec= - Added titles for commands on runtime
    set lined= - Minor bug fixes and improvements to runtime
    set linee= - Reworked color scheme setter
    set linef= - A handful of UI adjustments and improvements
    set lineg=echo More info: pastebin.com/JAX6fVnc
    set lineh=echo ==========================================================
    set linei=
    set linej=

---
+++ updatechk.bat
@@ -0,0 +1,12 @@
+set newver=12
+set rawver=12
+set linea===========================================================
+set lineb=Notes:
+set linec= - Added titles for commands on runtime
+set lined= - Minor bug fixes and improvements to runtime
+set linee= - Reworked color scheme setter
+set linef= - A handful of UI adjustments and improvements
+set lineg=echo More info: pastebin.com/JAX6fVnc
+set lineh=echo ==========================================================
+set linei=
+set linej=
